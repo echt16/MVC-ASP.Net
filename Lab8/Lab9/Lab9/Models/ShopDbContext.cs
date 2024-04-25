dbc3a006afc4184eb364b260c0573a4b88daf036
echt16 <albertivanov375@gmail.com>
2024-04-25 12:05:56 +0300

    Lab9 Commit

---
+++ Lab8/Lab9/Lab9/Models/ShopDbContext.cs
@@ -0,0 +1,9 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+namespace Lab9.Models
+{
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options) { Database.EnsureCreated(); }
+    }
+}
